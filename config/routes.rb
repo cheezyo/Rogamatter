5816e468b13e222b62a5b4ee6b8a39b61c3f7591
Cezar Johansson <cezarsincan@gmail.com>
2020-03-26 19:58:58 +0100

    sd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :mats
   resources :company_mats
- root :to => 'companies#index'
+ root :to => 'pages#index'
   resources :companies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
