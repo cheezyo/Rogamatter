9cef065673e8c1875192439448515bfb87486cd2
Cezar Johansson <cezarsincan@gmail.com>
2020-03-26 20:27:29 +0100

    sd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,8 @@ class PagesController < ApplicationController
     	
   	end
 
+  	def dashboard
+
+  	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :mats
+  
   resources :company_mats
  root :to => 'pages#index'
   resources :companies
+
+resources :pages do 
+	collection do 
+		get :dashboard
+end
+end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
